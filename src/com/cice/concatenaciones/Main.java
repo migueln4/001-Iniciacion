2093ae5ac26e7af4ab943afc01ffae01882250e0
Miguel <miguel444666@gmail.com>
2018-05-23 19:35:57 +0200

    Completada la información del cuaderno.

--- src/com/cice/concatenaciones/Main.java
+++ src/com/cice/concatenaciones/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
         int a = 7;
         //Tres ejemplos del cuaderno
-        (a*2+6/5)
+        System.out.println("El resultado es "+a*2+6/7); //Lo que hace es multiplicar 7 por 2 y concatenarlo a la división
+        System.out.println("El resultado es "+a*(2+6)/7);//Lo que hace es la suma, la multiplicación y la división
+        System.out.println("El resultado es "+(a*2+6/5));//Lo que hace es la operación completa y la suma de la multiplicación por la división
     }
 }
