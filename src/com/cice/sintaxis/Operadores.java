748ceffc810fcf4cf7bc28c692c980ce4fe0d51a
Miguel <miguel@yo.com>
2018-05-28 19:45:26 +0200

    Completada la información del cuaderno.

---
+++ src/com/cice/sintaxis/Operadores.java
@@ -0,0 +1,33 @@
+package com.cice.sintaxis;
+
+/**
+ * A partir de aquí se van a ver los operadores de relación, los lógicos, etcétera. Es el punto 2 del tema 1 de
+ * los apuntes de clase.
+ * */
+
+public class Operadores {
+
+    //OPERADORES DE RELACIÓN
+
+    // >    <   <=  >=  !=  ==
+
+    //La finalidad de los operadores de relación es conseguir una respuesta TRUE o FALSE
+
+    static boolean r1 = 1 > 2;
+    static boolean r2 = 1 < 2;
+    static boolean r3 = 1 >= 2;
+    static boolean r4 = 1 <= 2;
+    static boolean r5 = 1 != 2;
+    static boolean r6 = 1 == 2;
+
+    public static void main(String[] args) {
+
+        System.out.println(r1);
+        System.out.println(r2);
+        System.out.println(r3);
+        System.out.println(r4);
+        System.out.println(r5);
+        System.out.println(r6);
+
+    }
+}
