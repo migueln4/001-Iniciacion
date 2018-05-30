949c0a9dc57f54e86606eb684057ac46dc030e5b
Miguel <Miguel@30052018>
2018-05-30 21:50:17 +0200

    Completada la información del cuaderno.

---
+++ src/com/cice/sintaxis/InstruccionesIteracion.java
@@ -0,0 +1,15 @@
+package com.cice.sintaxis;
+
+public class InstruccionesIteracion {
+
+    public static void main(String[] args) {
+
+    /*BUCLE FOR
+    for (inicialización ; condición ; incremento)
+    */
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Esta es la vuelta " + (i + 1) + " porque i tiene un valor de " + i);
+        }
+    }
+}
---
+++ src/com/cice/sintaxis/ejercicio4/Main.java
@@ -0,0 +1,12 @@
+package com.cice.sintaxis.ejercicio4;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner
+
+
+    }
+
+}
