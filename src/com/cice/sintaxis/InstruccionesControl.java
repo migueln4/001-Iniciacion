8010c94a0ecaaea28c7d7715cfb7c7ad8d4344fd
Miguel <Miguel@30052018>
2018-05-30 19:51:16 +0200

    Completada la información del cuaderno.

---
+++ src/com/cice/sintaxis/InstruccionesControl.java
@@ -0,0 +1,18 @@
+package com.cice.sintaxis;
+
+public class InstruccionesControl {
+    public static void main(String[] args) {
+
+        //Estructura de control IF ELSE
+
+        if(true) {
+            //si se cumple la condición, se ejecuta este bloque de código
+            System.out.println("Condición cumplida.");
+        } else {
+            //Si no se cumple, ejecutamos este otro bloque de código
+            System.out.println("No se ha cumplido la función.");
+        }
+
+
+    }
+}
--- src/com/cice/sintaxis/Sacos.java
+++ src/com/cice/sintaxis/Sacos.java
@@ -1,5 +1,7 @@
 package com.cice.sintaxis;
 
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
 public class Sacos {
 
     static int[] numeros; //Declaración del array
@@ -18,6 +20,27 @@ public class Sacos {
         System.out.println(size);
 
         System.out.println(numeros2[0]);
+
+        //-------ARRAYS MULTIDIMENSIONALES-------------
+
+        System.out.println("------ARRAY MULTIDIMENSIONALES--------");
+
+        int[][] arrayBidimensional = new int[2][4];
+        int[] arrayB2[] = new int[2][4];
+        int arrayB3[][] = {{2,4,6,8},{1,3,5,7},{0,1,2,3},{9,8,7,6}}; //Al contrario que las otras dos, esta de de 4x4. Las otras dos son de 2x4.
+        //La posición [3][2] es 7
+        //La posición [1][0] es 1
+        System.out.println(arrayB3[3][2]);
+        System.out.println(arrayB3[1][0]);
+
+        int[][][] arrayTridimensional = {{{1,2},{2,4}},{{5,4},{8,4}}}; //Los arrays tridimensionales contienen arrays
+        // bidiemensionales.
+        //La posición [0][1][1] es 4
+        //La posición [1][0][1] es 4 también
+        System.out.println(arrayTridimensional[0][1][1]);
+        System.out.println(arrayTridimensional[1][0][1]);
+        int[][][] arrayT2 = new int[2][2][2]; //Tiene el mismo tamaño que el array tridimensional anterior.
+
     }
 
 }
