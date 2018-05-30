447644407ee5bbe86a76f1821b74e1fec0dcdaef
Miguel <Miguel@30052018>
2018-05-30 20:25:47 +0200

    Completada la información del cuaderno.

--- src/com/cice/sintaxis/ejercicio2/Main.java
+++ src/com/cice/sintaxis/ejercicio2/Main.java
@@ -1,5 +1,7 @@
 package com.cice.sintaxis.ejercicio2;
 
+import java.util.Scanner;
+
 /*
     Realizar un programa que, dado un determinado número entero almacenado en una variable nos muestre un mensaje en
     la consola indicando si el número es par o impar.
@@ -9,12 +11,21 @@ package com.cice.sintaxis.ejercicio2;
 
 public class Main {
     public static void main(String[] args) {
-        int n = 0;
 
-        if (n%2 == 0) {
-            System.out.println("El número "+n+" es par.");
+        int n = 3;
+        int entradaTeclado;
+
+        //Lo que hay que hacer para leer desde consola lo que se introduce con el teclado. OJO, no olvidar el IMPORT
+        // de arriba.
+
+        Scanner leerConsola = new Scanner(System.in);
+        System.out.println("Por favor, introduce un número:  ");
+        entradaTeclado = leerConsola.nextInt();
+
+        if (entradaTeclado % 2 == 0) {
+            System.out.println("El número " + entradaTeclado + " es par.");
         } else {
-            System.out.println("El número "+n+" es impar.");
+            System.out.println("El número " + entradaTeclado + " es impar.");
         }
     }
 }
