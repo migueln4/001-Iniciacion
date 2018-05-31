1fd7152cd0f21cf73f1f0e57248a9a0abd774b2f
Miguel <miguel@31052018>
2018-05-31 19:10:16 +0200

    Completada la información del cuaderno.

--- src/com/cice/sintaxis/ejercicio4/Main.java
+++ src/com/cice/sintaxis/ejercicio4/Main.java
@@ -4,8 +4,17 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner
+        int entradaTeclado;
+        long resultado=1;
+        Scanner leerConsola = new Scanner(System.in);
+        System.out.println("Por favor, introduce un número:  ");
+        entradaTeclado = leerConsola.nextInt();
 
+        for (int i=entradaTeclado; i>0; i--) {
+            resultado *= i;
+        }
+
+        System.out.println(entradaTeclado+"! = "+resultado);
 
     }
 
