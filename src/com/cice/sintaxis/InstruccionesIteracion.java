dd4b17e153ef1ede25f8ae26dbf65f066201bab7
Miguel <miguel@04062018>
2018-06-04 20:25:08 +0200

    Hechos hasta el ejercicio: 4

--- src/com/cice/sintaxis/InstruccionesIteracion.java
+++ src/com/cice/sintaxis/InstruccionesIteracion.java
@@ -11,5 +11,20 @@ public class InstruccionesIteracion {
         for (int i = 0; i < 10; i++) {
             System.out.println("Esta es la vuelta " + (i + 1) + " porque i tiene un valor de " + i);
         }
+
+/*
+
+Esto es un bucle infinito, pero compila.
+
+for (;;) {
+        }
+
+        */
+
+        int i = 3;
+        for (; i > 0; ) {
+            System.out.println("Hola, chato.");
+            i--;
+        }
     }
 }
--- src/com/cice/sintaxis/ejercicio4/Main.java
+++ src/com/cice/sintaxis/ejercicio4/Main.java
@@ -10,17 +10,29 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        int entradaTeclado;
+        int entradaTeclado = leer();
         long resultado = 1;
-        Scanner leerConsola = new Scanner(System.in);
-        System.out.println("Por favor, introduce un número:  ");
-        entradaTeclado = leerConsola.nextInt();
-        for (int i = entradaTeclado; i > 0; i--) {
-            resultado *= i;
+        if (entradaTeclado < 0) {
+            System.out.println("No existe el factorial de un número negativo.");
+        } else {
+            for (int i = entradaTeclado; i > 0; i--) {
+                resultado *= i;
+                if (resultado <= 0) {
+                    break;
+                }
+            }
+            if (resultado > 0) {
+                System.out.println(entradaTeclado + "! = " + resultado);
+            } else {
+                System.out.println("El cálculo se ha salido de rango.");
+            }
         }
-
-        System.out.println(entradaTeclado + "! = " + resultado);
 
     }
 
+    public static int leer() {
+        Scanner leerConsola = new Scanner(System.in);
+        System.out.print("Por favor, introduce un número:  ");
+        return leerConsola.nextInt();
+    }
 }
