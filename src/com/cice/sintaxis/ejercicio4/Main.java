d25f993388d58cc2d7815082a12a1f6382254d7f
Miguel <miguel@04062018>
2018-06-04 19:28:10 +0200

    Completada la información del cuaderno.

--- src/com/cice/sintaxis/ejercicio4/Main.java
+++ src/com/cice/sintaxis/ejercicio4/Main.java
@@ -1,20 +1,25 @@
 package com.cice.sintaxis.ejercicio4;
+
+/*
+    Realizar un programa que calcule el factorial de un número existente en una determinada variable entera.
+
+    */
+
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         int entradaTeclado;
-        long resultado=1;
+        long resultado = 1;
         Scanner leerConsola = new Scanner(System.in);
         System.out.println("Por favor, introduce un número:  ");
         entradaTeclado = leerConsola.nextInt();
-
-        for (int i=entradaTeclado; i>0; i--) {
+        for (int i = entradaTeclado; i > 0; i--) {
             resultado *= i;
         }
 
-        System.out.println(entradaTeclado+"! = "+resultado);
+        System.out.println(entradaTeclado + "! = " + resultado);
 
     }
 
